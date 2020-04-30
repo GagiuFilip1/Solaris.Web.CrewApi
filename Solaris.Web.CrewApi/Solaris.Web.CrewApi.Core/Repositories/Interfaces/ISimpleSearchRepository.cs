11301ea7933421c0884147341a07a026311c2cac
Filip <gagiu.filip@gmail.com>
2020-04-30 22:06:33 +0300

    CRWAPI-2 Added SimpleSearchRepository

---
+++ Solaris.Web.CrewApi/Solaris.Web.CrewApi.Core/Repositories/Interfaces/ISimpleSearchRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Solaris.Web.CrewApi.Core.Models.Helpers;
+using Solaris.Web.CrewApi.Core.Models.Interfaces;
+
+namespace Solaris.Web.CrewApi.Core.Repositories.Interfaces
+{
+    public interface ISimpleSearchRepository <TEntity> where TEntity : IIdentifier
+    {
+        Task<Tuple<int, List<TEntity>>> SearchAsync(Pagination pagination, Ordering ordering, IFilter<TEntity> filtering);
+        Task<int> CountAsync(IFilter<TEntity> filtering);
+    }
+}
---
+++ Solaris.Web.CrewApi/Solaris.Web.CrewApi.Infrastructure/Repositories/Implementations/CrewMemberRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Solaris.Web.CrewApi.Core.Extensions;
+using Solaris.Web.CrewApi.Core.Models.Entities;
+using Solaris.Web.CrewApi.Core.Models.Helpers;
+using Solaris.Web.CrewApi.Core.Models.Interfaces;
+using Solaris.Web.CrewApi.Core.Repositories.Interfaces;
+using Solaris.Web.CrewApi.Infrastructure.Data;
+
+namespace Solaris.Web.CrewApi.Infrastructure.Repositories.Implementations
+{
+    public class CrewMemberRepository : ICrewMemberRepository
+    {
+        private readonly DataContext m_dataContext;
+
+        public CrewMemberRepository(DataContext dataContext)
+        {
+            m_dataContext = dataContext;
+        }
+
+        public async Task<Tuple<int, List<CrewMember>>> SearchAsync(Pagination pagination, Ordering ordering, IFilter<CrewMember> filtering)
+        {
+            return await filtering.Filter(m_dataContext.CrewMembers.AsQueryable())
+                .WithOrdering(ordering, new Ordering())
+                .WithPaginationAsync(pagination);
+        }
+
+        public async Task<int> CountAsync(IFilter<CrewMember> filtering)
+        {
+            return await filtering.Filter(m_dataContext.CrewMembers.AsQueryable())
+                .CountAsync();
+        }
+    }
+}
