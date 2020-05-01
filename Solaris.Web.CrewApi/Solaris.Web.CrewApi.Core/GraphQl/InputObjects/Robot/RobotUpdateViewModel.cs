bb4e920e6ce6c12550d390cdc20282e0a0106889
Filip <gagiu.filip@gmail.com>
2020-05-01 13:12:02 +0300

    CRWAPI-4 Added Robot View Models

---
+++ Solaris.Web.CrewApi/Solaris.Web.CrewApi.Core/GraphQl/InputObjects/Robot/RobotUpdateViewModel.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+
+namespace Solaris.Web.CrewApi.Core.GraphQl.InputObjects.Robot
+{
+    public class RobotUpdateViewModel : InputObjectGraphType<Models.Entities.Robot>
+    {
+        public RobotUpdateViewModel()
+        {
+            Field(x => x.Id, type: typeof(NonNullGraphType<IdGraphType>)).Description("The id of the robot to be updated");
+            Field(x => x.Name);
+            Field(x => x.CreationDate).Description("The date this robot was created");
+            Field(x => x.ProductNumber).Description("The robot Product Number");
+            Field(x => x.CurrentStatus).Description("The current robot status (Free, Occupied, Broken)");
+            Field(x => x.ExplorersTeamId, type: typeof(NonNullGraphType<IdGraphType>)).Description("The id of the explorers team this Robot is assigned to");
+        }
+    }
+}
---
+++ Solaris.Web.CrewApi/Solaris.Web.CrewApi.Core/GraphQl/OutputObjects/Robot/RobotQueryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL.Types;
+
+namespace Solaris.Web.CrewApi.Core.GraphQl.OutputObjects.Robot
+{
+    public class RobotQueryViewModel : ObjectGraphType<Models.Entities.Robot>
+    {
+        public RobotQueryViewModel()
+        {
+            Field(x => x.Id, type: typeof(NonNullGraphType<GuidGraphType>));
+            Field(x => x.Name);
+            Field(x => x.CreationDate).Description("The date this robot was created");
+            Field(x => x.ProductNumber).Description("The robot Product Number");
+            Field(x => x.CurrentStatus).Description("The current robot status (Free, Occupied, Broken)");
+            Field(x => x.Type);
+            Field(x => x.ExplorersTeamId, type: typeof(NonNullGraphType<GuidGraphType>)).Description("The id of the explorers team this Robot is assigned to");
+        }
+    }
+}
